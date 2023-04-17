fc8873f2facb766561005e1e129d23ef25ca2387
Oscar Uriel Alcocer Delfin <isc18350287@gmail.com>
2023-04-17 10:03:14 -0600

    Nose que ponerle

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,114 @@
+import { useState } from "react"
 function App() {
+    const [itemChecked, setItemChecked] = useState({
+      first:false,
+      second:false,
+      third:false,
+    })
+    const handleItemChecked = (e) => {
+      // if (itemChecked[e.target.name]) {
+      //   setItemChecked({
+      //     ...itemChecked,
+      //     [e.target.name]: false,
+      //   });
+      // } else {
+      //   setItemChecked({
+      //     ...itemChecked,
+      //     [e.target.name]: true,
+      //   });
+      // }
+    setItemChecked({
+      ...itemChecked,
+      [e.target.name]: !itemChecked[e.target.name]
+    })
+    }
   return (
-    <div className="container text-center">
+  <div className="container text-center">
       <div className="row">
         <div className="col">
           <h1>Shopping List</h1>
           <hr />
         </div>
+        <div className="col text-end">
+          <button type="button" className="btn btn-outline-primary">
+            <i className="bi bi-plus-circle"></i>
+          </button>
+        </div>
       </div>
       <div className="row">
-        <div className="col">C</div>
-        <div className="col-8">Name item</div>
-        <div className="col-3">Buttons</div>
+        <div className="col">
+          <input
+          checked={itemChecked.first}
+          name="first"
+          onClick={(e) => handleItemChecked(e)}
+          type="checkbox"
+          />
+        </div>
+        <div className="col-2 text-start">
+          {
+            // itemChecked.first && <del>1 kg</del> 
+          }
+          {
+            // !itemChecked.first && "1 kg" 
+          }
+          {
+            itemChecked.first ? <del>1 kg</del> : "1 kg"
+          }
+        </div>
+        <div className="col-5 col-md-6 text-start"
+          style={{textDecoration: itemChecked.first && "line-through"}}
+          >
+            Tortillas
+          </div>
+        <div className="col-4 col-md-3 btn-group btn-group-sm" role="groups">
+          <button type="button" className="btn btn-outline-primary">
+            <i className="bi bi-pencil-square"></i>
+          </button>
+          <button type="button" className="btn btn-outline-primary">
+            <i className="bi bi-files"></i>
+          </button>
+          <button type="button" className="btn btn-outline-danger">
+            <i className="bi bi-trash2-fill"></i>
+          </button>
+        </div>
       </div>
       <div className="row">
-        <div className="col">C</div>
-        <div className="col-8">Name item</div>
-        <div className="col-3">Buttons</div>
+        <div className="col">
+          <input type="checkbox" />
+        </div>
+        <div className="col-2 text-start">1 Lt</div>
+        <div className="col-5 col-md-6 text-start">Aceite</div>
+        <div className="col-4 col-md-3 btn-group btn-group-sm" role="groups">
+          <button type="button" className="btn btn-outline-primary">
+            <i className="bi bi-pencil-square"></i>
+          </button>
+          <button type="button" className="btn btn-outline-primary">
+            <i className="bi bi-files"></i>
+          </button>
+          <button type="button" className="btn btn-outline-danger">
+            <i className="bi bi-trash2-fill"></i>
+          </button>
+        </div>
       </div>
       <div className="row">
-        <div className="col">C</div>
-        <div className="col-8">Name item</div>
-        <div className="col-3">Buttons</div>
+        <div className="col">
+          <input type="checkbox" />
+        </div>
+        <div className="col-2 text-start">3 Lt</div>
+        <div className="col-5 col-md-6 text-start">Coca Cola</div>
+        <div className="col-4 col-md-3 btn-group btn-group-sm" role="groups">
+          <button type="button" className="btn btn-outline-primary">
+            <i className="bi bi-pencil-square"></i>
+          </button>
+          <button type="button" className="btn btn-outline-primary">
+            <i className="bi bi-files"></i>
+          </button>
+          <button type="button" className="btn btn-outline-danger">
+            <i className="bi bi-trash2-fill"></i>
+          </button>
+        </div>
       </div>
     </div>
   )
 }
-
 export default App
